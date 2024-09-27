d3bec257ff0372e92a2be054bab23345624d4193
Radhika Kulkarni <rrkulkarni@tamu.edu>
2024-09-26 23:27:53 -0500

    save common used variables in beginning of file; add edge cases tests to check for compatibility of matrices, lambda val, eta val, etc and stop execution if necessary

--- FunctionsLR.R
+++ FunctionsLR.R
@@ -17,19 +17,39 @@
 # error_test - (numIter + 1) length vector of testing error % at each iteration (+ starting value)
 # objective - (numIter + 1) length vector of objective values of the function that we are minimizing at each iteration (+ starting value)
 LRMultiClass <- function(X, y, Xt, yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = NULL){
+  n <- nrow(X) # save variable of nrows as n, number of observations
+  p <- ncol(X) # save variable of ncols as p, number of predictors
+  ntest <- nrow(Xt) # save variable of nrows of Xtest as ntest (num obs of Xtest)
+  tX <- t(X) # compute transpose of X once to be accessed
+  
   ## Check the supplied parameters as described. You can assume that X, Xt are matrices; y, yt are vectors; and numIter, eta, lambda are scalars. You can assume that beta_init is either NULL (default) or a matrix.
   ###################################
-  # Check that the first column of X and Xt are 1s, if not - display appropriate message and stop execution.
-  
+  # Check that the first column of X is 1s, if not - display appropriate message and stop execution.
+  if((X[ , 1] != rep(1,n)) ){
+    stop("Error: check that the first column of X is 1s.")
+  }
+  # Check that the first column of Xt is 1s, if not - display appropriate message and stop execution.
+  if((Xt[ , 1] != rep(1,ntest)) ){
+    stop("Error: check that the first column of X test is 1s.")
+  }
   # Check for compatibility of dimensions between X and Y
-  
-  # Check for compatibility of dimensions between Xt and Yt
-  
+  if(n != nrow(Y)){
+    stop("Error: check that the dimensions of X and Y are compatible for matrix multiplication.")
+  }
+  # Check for compatibility of dimensions between Xt and Yt - CHECK THIS!!
+  if(ntest != nrow(Ytest)){
+    stop("Error: check that the dimensions of Xtest and Ytest are compatible for matrix multiplication.")
+  }
   # Check for compatibility of dimensions between X and Xt
   
   # Check eta is positive
-  
+  if(eta <= 0){
+    stop("Error: Eta must be positive! Change your value of eta.")
+  }
   # Check lambda is non-negative
+  if(lambda < 0){
+    stop("Error: lambda must be nonnegative! Change your value of lambda.")
+  }
   
   # Check whether beta_init is NULL. If NULL, initialize beta with p x K matrix of zeroes. If not NULL, check for compatibility of dimensions with what has been already supplied.
   
