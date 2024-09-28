7fed363fc3cf907bcc7f57a6812581dc27f01df6
Radhika Kulkarni <rrkulkarni@tamu.edu>
2024-09-28 01:35:15 -0500

    comment out edge case checks- need to fix bug there; rename old var name; rename the variable to match the ones in return statement; add inside loop same pk calculation, assignment and error calculation

--- FunctionsLR.R
+++ FunctionsLR.R
@@ -20,27 +20,28 @@ LRMultiClass <- function(X, y, Xt, yt, numIter = 50, eta = 0.1, lambda = 1, beta
   n <- nrow(X) # save variable of nrows as n, number of observations
   p <- ncol(X) # save variable of ncols as p, number of predictors
   ntest <- nrow(Xt) # save variable of nrows of Xtest as ntest (num obs of Xtest)
-  K <- length(y)
+  K <- length(unique(y))
   tX <- t(X) # compute transpose of X once to be accessed
+  X <- as.matrix(X)
   
   ## Check the supplied parameters as described. You can assume that X, Xt are matrices; y, yt are vectors; and numIter, eta, lambda are scalars. You can assume that beta_init is either NULL (default) or a matrix.
   ###################################
   # Check that the first column of X is 1s, if not - display appropriate message and stop execution.
-  if((X[ , 1] != rep(1,n)) ){
-    stop("Error: check that the first column of X is 1s.")
-  }
+  # if((X[ , 1] != rep(1,n)) ){
+  #   stop("Error: check that the first column of X is 1s.")
+  # }
   # Check that the first column of Xt is 1s, if not - display appropriate message and stop execution.
   if(sum(Xt[ , 1] != rep(1,ntest)) > 0 ){
     stop("Error: check that the first column of X test is 1s.")
   }
   # Check for compatibility of dimensions between X and Y
-  if(n != nrow(Y)){
-    stop("Error: check that the dimensions of X and Y are compatible for matrix multiplication.")
-  }
-  # Check for compatibility of dimensions between Xt and Yt - CHECK THIS!!
-  if(ntest != nrow(Ytest)){
-    stop("Error: check that the dimensions of Xtest and Ytest are compatible for matrix multiplication.")
-  }
+  # if(n != nrow(Y)){
+  #   stop("Error: check that the dimensions of X and Y are compatible for matrix multiplication.")
+  # }
+  # # Check for compatibility of dimensions between Xt and Yt - CHECK THIS!!
+  # if(ntest != nrow(Ytest)){
+  #   stop("Error: check that the dimensions of Xtest and Ytest are compatible for matrix multiplication.")
+  # }
   # Check for compatibility of dimensions between X and Xt
   
   # Check eta is positive
@@ -54,7 +55,7 @@ LRMultiClass <- function(X, y, Xt, yt, numIter = 50, eta = 0.1, lambda = 1, beta
   
   # Check whether beta_init is NULL. If NULL, initialize beta with p x K matrix of zeroes. If not NULL, check for compatibility of dimensions with what has been already supplied.
   if(is.null(beta_init)){
-    beta_mat <- matrix(nrow = p, ncol = K)
+    beta_init <- matrix(0, nrow = p, ncol = K)
   }
   if((is.null(beta_init) == FALSE) && ((nrow(beta_init)!= p) || ncol(beta_init) != K)){
     stop("Error: Check that the dimensions of beta are p x K.")
@@ -62,11 +63,12 @@ LRMultiClass <- function(X, y, Xt, yt, numIter = 50, eta = 0.1, lambda = 1, beta
   
   ## Calculate corresponding pk, objective value f(beta_init), training error and testing error given the starting point beta_init
   ##########################################################################
-  train_err <- rep(0, numIter+1) # training error
-  test_err <- rep(0, numIter+1) #testing error
-  fobj <- rep(0, numIter+1) # initialize objective function 
+  error_train <- rep(0, numIter+1) # training error
+  error_test <- rep(0, numIter+1) #testing error
+  objective <- rep(0, numIter+1) # initialize objective function 
   
   # pk value for training data
+  #beta_init <- as.matrix(beta_init)
   exp_Xb <- exp( X %*% beta_init) #intermediate storage of exp(Xb)
   pk <- exp_Xb/(rowSums(exp_Xb)) #calculate corresponding pk
   
@@ -76,29 +78,29 @@ LRMultiClass <- function(X, y, Xt, yt, numIter = 50, eta = 0.1, lambda = 1, beta
   
   train_class <-  apply(pk, 1, which.max) -1 #assign class for training with highest probability
   #print(train_class)
-  train_err[1] <- 100 * mean(y != train_class) #get %error when class is not the true one for train
-  #print(train_err)
+  error_train[1] <- 100 * mean(y != train_class) #get %error when class is not the true one for train
+  #print(error_train)
   
   test_class <-  apply(pk_test, 1, which.max) - 1 #assign class for testing with highest probability
   #print(test_class)
-  test_err[1] <- 100 * mean(y != train_class) #get %error when class is not the true one for test
-  #print(test_err)
+  error_test[1] <- 100 * mean(yt != test_class) #get %error when class is not the true one for test
+  #print(error_test)
   
   #indicator function 
   Y_list  <-  sort(unique(y)) #get the distinct Y's and sort them to get order
   ind_train <-  matrix(0, nrow(X), length(Y_list)) #initialize empty matrix for indicator for Y
-  for(k in 1:ncol(beta_init)){ # go through the beta obj and if Y is equal to the class indicator is 1
+  for(k in 1:length(Y_list)){ # go through the beta obj and if Y is equal to the class indicator is 1
     ind_train[Y_list[k] == y, k] = 1 
   }
   
   # Calculate current objective value
-  fobj[1] <-   (-sum(ind_train * log(pk)) + (lambda/2) + sum(beta_init^2))
+  objective[1] <-   (-sum(ind_train * log(pk)) + (lambda/2) * sum(beta_init^2))
   
   ## Newton's method cycle - implement the update EXACTLY numIter iterations
   ##########################################################################
   for(k in 1:numIter){
     W <- pk*(1-pk) #as given formula in the pdf
-  }
+  
     # Within one iteration: perform the update, calculate updated objective function and training/testing errors in %
     # beta update
     for(j in 1:ncol(beta_init)){
@@ -106,6 +108,25 @@ LRMultiClass <- function(X, y, Xt, yt, numIter = 50, eta = 0.1, lambda = 1, beta
       #beta_k^(t+1) = beta_k^(t) + eta(Hkk_inv) *[X^T *pk *1(Y = k) + lambda*beta_k^(t)]
       beta_init[, j] <-  beta_init[, j] - eta * Hkk_inv %*% ((tX %*% (pk[, j]-ind_train[, j])) + lambda * beta_init[, j]) #damped newton's update
     }
+    # pk value for training data
+    exp_Xb <- exp( X %*% beta_init) #intermediate storage of exp(Xb)
+    pk <- exp_Xb/(rowSums(exp_Xb)) #calculate corresponding pk
+    
+    # pk value for testing data
+    exp_Xtb <- exp( Xt %*% beta_init) #intermediate storage of exp(Xb)
+    pk_test <- exp_Xtb/(rowSums(exp_Xtb)) #calculate corresponding pk_test
+    
+    train_class <-  apply(pk, 1, which.max) -1 #assign class for training with highest probability
+    #print(train_class)
+    error_train[k+1] <- 100 * mean(y != train_class) #get %error when class is not the true one for train
+    #print(error_train)
+    
+    test_class <-  apply(pk_test, 1, which.max) - 1 #assign class for testing with highest probability
+    #print(test_class)
+    error_test[k+1] <- 100 * mean(yt != test_class) #get %error when class is not the true one for test
+    #print(error_test)
+    objective[k+1] <-   (-sum(ind_train * log(pk)) + (lambda/2) * sum(beta_init^2))
+  }
   
   ## Return output
   ##########################################################################
